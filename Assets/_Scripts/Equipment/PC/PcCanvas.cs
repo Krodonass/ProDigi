92c30299903915abb711d85d8bd373bf96139b8d
HappyMirandum <u37773@hs-harz.de>
2025-03-18 18:46:46 +0100

    PC-Icons and Sprites integrated/adjusted + MyMail open and close function adjusted and small general funktionality adjustments + Controlsscreen can be opened and closed

--- Assets/_Scripts/Equipment/PC/PcCanvas.cs
+++ Assets/_Scripts/Equipment/PC/PcCanvas.cs
@@ -9,6 +9,7 @@ public class PcCanvas : MonoBehaviour
 
     private Canvas canvas;
 
+    [SerializeField] private GameObject ControlsScreen;
     [SerializeField] private GameObject MailScreen;
     [SerializeField] private GameObject StartScreen;
 
@@ -27,17 +28,11 @@ public class PcCanvas : MonoBehaviour
             //canvas.worldCamera = Camera.current;
         }
 
+        ControlsScreen.SetActive(false);
         MailScreen.SetActive(false);
         StartScreen.SetActive(true);
     }
 
-    private void Update()
-    {
-        if (Input.GetKey(KeyCode.Mouse0))
-        {
-            MouseClick.Play();
-        }
-    }
 
     // Update is called once per frame
     public void PcQuit()
@@ -51,23 +46,41 @@ public class PcCanvas : MonoBehaviour
     }
 
         //Opens the Mail overview
-    public void OnMailUI()
+    public void OpenMailScreen()
     {
+        ControlsScreen.SetActive(false);
         StartScreen.SetActive(false);
         MailScreen.SetActive(true);
     }
 
 
     //Goes back to Mainmenu
-    public void OpenMainMenu()
+    public void OpenStartScreen()
     {
         MailScreen.SetActive(false);
+        ControlsScreen.SetActive(false);
         StartScreen.SetActive(true);
     }
+
+
+
+    public void OpenControlsScreen()
+    {
+        MailScreen.SetActive(false);
+        StartScreen.SetActive(false);
+        ControlsScreen.SetActive(true);
+    }
+
 
     //Opens a specific mail
     public void OpenMail()
     {
 
+    }
+
+
+    public void MouseClickSound()
+    {
+        MouseClick.Play();
     }
 }
