aaef374c672b6f9ff804bfbbec58c5d895e4bc06
HappyMirandum <u37773@hs-harz.de>
2025-06-04 09:54:59 +0200

    first quest trigger fixed

--- Assets/_Scripts/Equipment/GloveBox/VacuumChamberChecker.cs
+++ Assets/_Scripts/Equipment/GloveBox/VacuumChamberChecker.cs
@@ -12,8 +12,7 @@ public class VacuumChamberChecker : MonoBehaviour
     private void OnCollisionEnter(Collision col)
     {
         if (col.gameObject == batteryParts[0] || col.gameObject == batteryParts[1] || col.gameObject == batteryParts[2] || col.gameObject == batteryParts[3] 
-            || col.gameObject == batteryParts[4] || col.gameObject == batteryParts[5] || col.gameObject == batteryParts[6] || col.gameObject == batteryParts[7]
-            || col.gameObject == batteryParts[8] )
+            || col.gameObject == batteryParts[4] || col.gameObject == batteryParts[5] || col.gameObject == batteryParts[6] || col.gameObject == batteryParts[7])
         {
             Debug.Log("Batterieteil in Vakuumkammer gepackt!");
             batteryPartsCount++;
@@ -23,8 +22,7 @@ public class VacuumChamberChecker : MonoBehaviour
     private void OnCollisionExit(Collision col)
     {
         if (col.gameObject == batteryParts[0] || col.gameObject == batteryParts[1] || col.gameObject == batteryParts[2] || col.gameObject == batteryParts[3] 
-            || col.gameObject == batteryParts[4] || col.gameObject == batteryParts[5] || col.gameObject == batteryParts[6] || col.gameObject == batteryParts[7]
-            || col.gameObject == batteryParts[8] )
+            || col.gameObject == batteryParts[4] || col.gameObject == batteryParts[5] || col.gameObject == batteryParts[6] || col.gameObject == batteryParts[7])
         {
             Debug.Log("Batterieteil aus Vakuumkammer rausgenommen!");
             batteryPartsCount--;
